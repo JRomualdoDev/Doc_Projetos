e6b89bc20317c16a87172bb330b02857d361f2af
Junior <jromualdo3@hotmail.com>
2024-06-29 00:43:52 -0300

    links github

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,7 +1,19 @@
 export default {
-    logo: <span>Projetos Documentos</span>,
     project: {
-        link: 'https://github.com/shuding/nextra'
-    }
+        link: 'https://github.com/JRomualdoDev'
+    },
+    logo: (
+        <>
+            {/* <svg width="24" height="24" viewBox="0 0 24 24">
+                <path
+                    fill="currentColor"
+                    d="M21.5,21.5v129h64.5v-32.25v-64.5v-32.25zM86,53.75c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25c-17.7805,0 -32.25,14.4695 -32.25,32.25zM118.25,86c-17.7805,0 -32.25,14.4695 -32.25,32.25c0,17.7805 14.4695,32.25 32.25,32.25c17.7805,0 32.25,-14.4695 32.25,-32.25c0,-17.7805 -14.4695,-32.25 -32.25,-32.25z"
+                ></path>
+            </svg> */}
+            <span style={{ marginLeft: '.4em', fontWeight: 800 }}>
+                Meus Projetos
+            </span>
+        </>
+    ),
     // ... other theme options
 }
