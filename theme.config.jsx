3c7729a54697d52bb164a65db384ad3b0bcd13e9
Junior <jromualdo3@hotmail.com>
2024-07-02 22:40:01 -0300

    melhorias

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,3 +1,6 @@
+import { useRouter } from 'next/router'
+import { useConfig } from 'nextra-theme-docs'
+
 export default {
     project: {
         link: 'https://github.com/JRomualdoDev'
@@ -15,5 +18,37 @@ export default {
             </span>
         </>
     ),
-    // ... other theme options
+    head: () => {
+        const { asPath, defaultLocale, locale } = useRouter()
+        const { frontMatter } = useConfig()
+        const url =
+            'https://github.com/JRomualdoDev' +
+            (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+        return (
+            <>
+                <meta property="og:url" content={url} />
+                <meta property="og:title" content={frontMatter.title || 'Nextra'} />
+                <meta
+                    property="og:description"
+                    content={frontMatter.description || 'The next site builder'}
+                />
+            </>
+        )
+    },
+    navigation: {
+        prev: true,
+        next: true
+    },
+    footer: {
+        text: (
+            <span>
+                MIT {new Date().getFullYear()} ©{' '}
+                <a href="https://github.com/JRomualdoDev" target="_blank">
+                    Github
+                </a>
+                .
+            </span>
+        )
+    }
 }
